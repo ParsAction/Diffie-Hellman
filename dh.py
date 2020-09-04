b7208beddc5637a914ae965febbffac15d2b5c40
ParsAction <53015276+ParsAction@users.noreply.github.com>
2020-09-04 22:46:09 +0430

    Update dh.py

--- dh.py
+++ dh.py
@@ -16,18 +16,28 @@ keyB=(A**b) % p
 
 
 #Modular Exponentiation Calculation 
-def MECalc(j, k, l):
-    J = j
-    K = k
-    Z = 1
-    while K > 0:
-        if K % 2 == 0:
-            J = (J * J) % l
-            K = K/2
-        else:
-            Z = (J * Z) % l
-            K = K - 1
-    return Z
+def MECalc(x, y, p):
+    res = 1     # Initialize result 
+  
+    # Update x if it is more 
+    # than or equal to p 
+    x = x % p  
+      
+    if (x == 0) : 
+        return 0
+  
+    while (y > 0) : 
+          
+        # If y is odd, multiply 
+        # x with result 
+        if ((y & 1) == 1) : 
+            res = (res * x) % p 
+  
+        # y must be even now 
+        y = y >> 1      # y = y/2 
+        x = (x * x) % p 
+          
+    return res
 
 
 
